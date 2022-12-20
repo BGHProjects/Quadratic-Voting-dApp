3015d5371ab17028d1912450c76246fecdf39da0
Brodie <brodie@labrys.io>
2022-12-20 20:57:12 +1000

    feat: :rocket: deployed contract to Avalanche Fuji

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({ path: __dirname + "/.env" });
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    avalancheTest: {
+      url: API_URL,
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
